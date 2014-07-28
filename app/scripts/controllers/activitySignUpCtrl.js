0c3eb3ef50e8e8e8760c4902bdfd80e3e7ecaf0d
ZhouYong10 <1059981087@qq.com>
2014-07-28 17:16:44 +0800

    第二张卡片，在报名页面点击报名开始报名，按钮变为结束。修改了一下活动列表页面的样式。

--- app/scripts/controllers/activitySignUpCtrl.js
+++ app/scripts/controllers/activitySignUpCtrl.js
@@ -2,10 +2,26 @@
  * Created by zhouyong on 14-7-19.
  */
 angular.module('partyBidApp')
-    .controller('activitySignUpCtrl',function($scope,$location){
+    .controller('activitySignUpCtrl',function($scope,$location) {
 
-        if(!localStorage.acts){
+        if (!localStorage.acts) {
             $location.path("/createActivity");
+        }
+
+        $scope.startEnd = "开始";
+
+        $scope.start_end = function(){
+
+            if($scope.startEnd == "开始"){
+
+                $scope.startEnd = "结束";
+                document.getElementById("startEnd").blur();
+
+            }else{
+
+                
+
+            }
         }
 
     });
